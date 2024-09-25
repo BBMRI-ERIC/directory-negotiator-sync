5c2d8e16fae97aeef832446dd78d24619e036678
asulis <alessandro.sulis@crs4.it>
2024-09-25 16:46:55 +0200

    feat: Implements networks synchronization

--- eu/bbmri/directorysync/negotiator_client.py
+++ eu/bbmri/directorysync/negotiator_client.py
@@ -3,7 +3,7 @@ import json
 import requests
 from pydantic import BaseModel
 
-from eu.bbmri.directorysync.models.dto.network import NegotiatorNetworkDTO
+from eu.bbmri.directorysync.models.dto.network import NegotiatorNetworkDTO, NetworkDirectoryDTO
 from eu.bbmri.directorysync.models.dto.organization import NegotiatorOrganizationDTO, OrganizationDirectoryDTO
 from eu.bbmri.directorysync.models.dto.resource import NegotiatorResourceDTO, ResourceDirectoryDTO
 from eu.config import NEGOTIATOR_TOKEN, NEGOTIATOR_API_URL, LOG
@@ -40,7 +40,7 @@ def create_resource_add_DTO(resource: ResourceDirectoryDTO, organization_id):
         'sourceId': resource.id,
         'description': resource.description,
         'organizationId': organization_id,
-        'accessFormId' : 1,
+        'accessFormId': 1,
         'discoveryServiceId': 1
     }
 
@@ -49,6 +49,24 @@ def add_resources (resources: list):
 
 
 def update_resource_name_or_description(id, name, description):
-    response = requests.patch(f'{NEGOTIATOR_API_URL}/resources/{id}', data=json.dumps({'name': name, 'description': description }), headers=get_auth_header())
-    print(response.status_code)
+    requests.patch(f'{NEGOTIATOR_API_URL}/resources/{id}', data=json.dumps({'name': name, 'description': description }), headers=get_auth_header())
+
+
+def create_network_add_DTO(network: NetworkDirectoryDTO):
+    return {
+        'externalId': network.id,
+        'name': network.name,
+        'contactEmail': network.contact.email,
+        'uri': network.url
+        }
+
+
+def add_networks(networks: list):
+    requests.post(f'{NEGOTIATOR_API_URL}/networks', data=json.dumps(networks), headers=get_auth_header())
+
+
+def update_network_info(id, name, url, email, external_id):
+    requests.put(f'{NEGOTIATOR_API_URL}/networks/{id}', data=json.dumps({'name': name, 'uri': url, 'contactEmail': email, 'externalId': external_id}), headers=get_auth_header())
+
+
 
--- eu/bbmri/sync_service.py
+++ eu/bbmri/sync_service.py
@@ -1,10 +1,12 @@
 import json
 
-from eu.bbmri.directorysync.directory_client import get_all_biobanks, get_all_collections
+from eu.bbmri.directorysync.directory_client import get_all_biobanks, get_all_collections, get_all_directory_networks
+from eu.bbmri.directorysync.models.dto.network import NetworkDirectoryDTO, NegotiatorNetworkDTO
 from eu.bbmri.directorysync.models.dto.organization import OrganizationDirectoryDTO, NegotiatorOrganizationDTO
 from eu.bbmri.directorysync.models.dto.resource import ResourceDirectoryDTO, NegotiatorResourceDTO
 from eu.bbmri.directorysync.negotiator_client import get_all_organizations, add_organizations, update_organization_name, \
-    create_resource_add_DTO, add_resources, get_all_resources, update_resource_name_or_description
+    create_resource_add_DTO, add_resources, get_all_resources, update_resource_name_or_description, \
+    get_all_negotiator_networks, create_network_add_DTO, add_networks, update_network_info
 from eu.config import LOG
 
 
@@ -29,6 +31,17 @@ def get_negotiator_resource_by_external_id(negotiator_resources: list[Negotiator
         raise Exception(f'More than one resource with the externalId {external_id} found in the Negotiator')
 
 
+def get_negotiator_network_by_external_id(negotiator_networks: list[NegotiatorNetworkDTO],
+                                               external_id: str):
+    network = list(filter(lambda item: item.externalId == external_id, negotiator_networks))
+    if len(network) == 1:
+        return network[0]
+    elif len(network) == 0:
+        return None
+    else:
+        raise Exception(f'More than one network with the externalId {external_id} found in the Negotiator')
+
+
 def create_sync_job():
     pass
 
@@ -40,6 +53,9 @@ def sync_all():
     negotiator_resources = get_all_resources()
     sync_all_organizations(directory_organizations, negotiator_organizations)
     sync_all_resources(directory_resources, negotiator_resources)
+    directory_networks = get_all_directory_networks()
+    negotiator_networks = get_all_negotiator_networks()
+    sync_all_networks(directory_networks, negotiator_networks)
 
 
 def sync_all_organizations(directory_organziations: list[OrganizationDirectoryDTO],
@@ -50,7 +66,7 @@ def sync_all_organizations(directory_organziations: list[OrganizationDirectoryDT
     for directory_organization in directory_organziations:
         external_id = directory_organization.id
         negotiation_organization = get_negotiator_organization_by_external_id(negotiator_organizations, external_id)
-        if (negotiation_organization):
+        if negotiation_organization:
             if negotiation_organization.name != directory_organization.name:
                 LOG.info(f'Updating name for organization: {external_id}')
                 update_organization_name(negotiation_organization.id, directory_organization.name, external_id)
@@ -87,3 +103,20 @@ def sync_all_resources(directory_resources: list[ResourceDirectoryDTO],
                                                     directory_resource.description)
     if len(resources_to_add) > 0:
         add_resources(resources_to_add)
+
+
+def sync_all_networks(directory_networks: list[NetworkDirectoryDTO], negotiator_networks: list[NegotiatorNetworkDTO]):
+    networks_to_add = list()
+    LOG.info("Starting adding or updating of networks")
+    for directory_network in directory_networks:
+        external_id = directory_network.id
+        network = get_negotiator_network_by_external_id(negotiator_networks, external_id)
+        if network:
+            if network.name != directory_network.name or network.uri != directory_network.url or network.contactEmail != directory_network.contact.email:
+                LOG.info(f'Updating mane and/or url and or contact email for network with external id: {external_id}')
+                update_network_info(network.id,directory_network.name, directory_network.url, directory_network.contact.email, external_id)
+        else:
+            LOG.info(f'Network with id {external_id} not fount, adding it to the list of networks to add')
+            networks_to_add.append(create_network_add_DTO(directory_network))
+    if len(networks_to_add) > 1:
+        add_networks(networks_to_add)
