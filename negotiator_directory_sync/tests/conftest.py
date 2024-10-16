347743fe0036bd68427372dbaa5d27832fabeff5
asulis <alessandro.sulis@crs4.it>
2024-10-16 15:19:59 +0200

    test: Realigns tests to the new configuration

--- negotiator_directory_sync/tests/conftest.py
+++ negotiator_directory_sync/tests/conftest.py
@@ -1,10 +1,25 @@
-import pytest, requests
-from negotiator_directory_sync.clients.negotiator_client import NegotiatorAPIClient
-from negotiator_directory_sync.auth import get_token
-from negotiator_directory_sync.clients.directory_client import get_all_biobanks, get_all_collections, get_all_directory_networks
+import os
+import pytest
+import requests
 
 NEGOTIATOR_API_URL = "http://localhost:8081/api/v3"
 DIRECTORY_API_URL = "http://localhost:8080/Directory/directory/graphql"
+AUTH_CLIENT_ID = "123"
+AUTH_CLIENT_SECRET = "123"
+AUTH_OIDC_TOKEN_URI = "http://localhost:4011/connect/token"
+JOB_SCHEDULE_INTERVAL = 20
+
+os.environ['DIRECTORY_API_URL'] = DIRECTORY_API_URL
+os.environ['NEGOTIATOR_API_URL'] = NEGOTIATOR_API_URL
+os.environ['AUTH_CLIENT_ID'] = AUTH_CLIENT_ID
+os.environ['AUTH_CLIENT_SECRET'] = AUTH_CLIENT_SECRET
+os.environ['AUTH_OIDC_TOKEN_URI'] = AUTH_OIDC_TOKEN_URI
+os.environ['JOB_SCHEDULE_INTERVAL'] = str(JOB_SCHEDULE_INTERVAL)
+
+from negotiator_directory_sync.clients.negotiator_client import NegotiatorAPIClient
+from negotiator_directory_sync.auth import get_token
+from negotiator_directory_sync.clients.directory_client import get_all_biobanks, get_all_collections, \
+    get_all_directory_networks
 
 
 def get_session():
@@ -35,9 +50,3 @@ def pytest_configure(config):
     pytest.directory_organizations = get_all_biobanks()
     pytest.directory_resources = get_all_collections()
     pytest.directory_networks = get_all_directory_networks()
-
-
-
-
-
-
