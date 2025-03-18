438987a1bbf415954cc8d19d6643ac062eb88b1f
asulis <alessandro.sulis@crs4.it>
2025-03-18 16:47:38 +0100

    fix: Fix negotiator client instantiation at the beginning of a chron job cycle, and when the microservice is started forces the execution of the first job cycle without having to wait for the interval

--- main.py
+++ main.py
@@ -2,23 +2,24 @@ from datetime import datetime
 
 import schedule
 
-from auth import get_token
+from auth import get_token, renew_access_token
 from clients.negotiator_client import NegotiatorAPIClient
 from config import LOG, NEGOTIATOR_API_URL, JOB_SCHEDULE_INTERVAL
 from synchronization.sync_service import sync_all
 
 
-def cron_job(negotiator_client):
+def cron_job():
     LOG.info(f"Starting cron job at: {datetime.now()}")
+    negotiator_client = NegotiatorAPIClient(NEGOTIATOR_API_URL, get_token())
     sync_all(negotiator_client)
 
 
 def sync_directory():
-    negotiator_client = NegotiatorAPIClient(NEGOTIATOR_API_URL, get_token())
-    schedule.every(int(JOB_SCHEDULE_INTERVAL)).seconds.do(cron_job, negotiator_client)
+    schedule.every(int(JOB_SCHEDULE_INTERVAL)).seconds.do(cron_job)
 
 
 def run_microservice():
+    cron_job()
     sync_directory()
     while True:
         schedule.run_pending()
