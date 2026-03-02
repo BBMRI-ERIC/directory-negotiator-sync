c12ac73c3b2f474e10970f2c8406b02d6bfbf76b
asulis <alessandro.sulis@crs4.it>
2026-03-02 09:28:57 +0100

    ci: Add multiple directory sources integration tests to the CI

--- tests/integration/integration_tests_multiple_directories.py
+++ tests/integration/integration_tests_multiple_directories.py
@@ -37,5 +37,36 @@ def test_common_biobank_all_sources_updated_by_source_1():
     assert updated_negotiator_organization.description == 'biobank 2 source 1 description'
 
 
+def test_common_biobank_all_sources_updated_by_source_2():
+    add_or_update_biobank(source_2_session, source_2_url, "bbmri-eric:ID:NL_biobank_upd_source_2", "pid_biobank_2_upd_s2_source_2", "biobank_2_updated_source_2_source_2_name",
+                          "biobank upd source 2 source 2 description", 'bbmri-eric:contactID:EU_network',
+                          'false', 'insert')
+
+    add_or_update_biobank(source_3_session, source_3_url, "bbmri-eric:ID:NL_biobank_upd_source_2", "pid_biobank_2_upd_s2_source_2",
+                          "biobank_2_updated_source_2_source_3_name",
+                          "biobank 2 source 3 description", 'bbmri-eric:contactID:EU_network',
+                          'false', 'update')
+
+    cron_job()
+    negotiator_organizations_after_sync = pytest.negotiator_client.get_all_organizations()
+    updated_negotiator_organization = \
+        [org for org in negotiator_organizations_after_sync if org.externalId == 'bbmri-eric:ID:NL_biobank_upd_source_2'][0]
+    assert updated_negotiator_organization.name == 'biobank_2_updated_source_2_source_2_name'
+    assert updated_negotiator_organization.description == 'biobank upd source 2 source 2 description'
+
+def test_biobank_all_sources_updated_by_source_3():
+    add_or_update_biobank(source_3_session, source_3_url, "bbmri-eric:ID:NL_biobank_source_3_exclusive", "pid_biobank_2_s3_exclusive",
+                          "biobank_2_updated_source_2_source_3_name",
+                          "biobank source 3 exclusive description", 'bbmri-eric:contactID:EU_network',
+                          'false', 'insert')
+
+    cron_job()
+    negotiator_organizations_after_sync = pytest.negotiator_client.get_all_organizations()
+    updated_negotiator_organization = \
+        [org for org in negotiator_organizations_after_sync if org.externalId == 'bbmri-eric:ID:NL_biobank_source_3_exclusive'][0]
+    assert updated_negotiator_organization.name == 'pid_biobank_2_s3_exclusive'
+    assert updated_negotiator_organization.description == 'biobank source 3 exclusive description'
+
+
 
 
